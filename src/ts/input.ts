8bf146d0f7619750d1d7ebf516dad09d9e677e76
lcluber <ludovic.cluber@gmail.com>
2018-07-25 21:15:00 +0200

    added package-lock.json

--- src/ts/input.ts
+++ src/ts/input.ts
@@ -1,30 +1,30 @@
 
 export class Input {
-  
-  defaultASCII : number; //first ascii code assigned  will be kept here
+
+  defaultASCII : number; //first ascii code assigned will be kept here
   callback : Function;
   scope : any;
-  
+
   constructor (ascii: number, callback: Function, scope: any) {
-  
+
     this.defaultASCII = ascii;
     this.callback     = callback;
     if( scope ){
       this.callback   = this.callback.bind( scope );
     }
-    
+
   }
-  
+
   setDefaultASCII(ascii:number) {
     this.defaultASCII = ascii;
   }
-  
+
   Down (a: KeyboardEvent) {
     //this.pushed=1;
     a.preventDefault();
     this.callback(true);
   }
-  
+
   Up () {
     //this.pushed=0;
     //if(this.listenUp)
